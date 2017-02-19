de98806db8d0b9d0b337a689d8f669cbc1213fc0
U-DESKTOP-97GR81P\suhas <suhas.servesh@gmail.com>
2017-02-19 15:53:31 -0500

    Added Resume Pages

---
+++ Website/angular/routing.js
@@ -0,0 +1,23 @@
+/**
+ * Created by suhas on 2/9/2017.
+ */
+(function(){
+    angular
+        .module("Sk-App", ["ngRoute"])
+        .config(function($routeProvider){
+            $routeProvider
+                .when("/", {
+                    templateUrl: "/angular/views/user/default.view.client.html",
+                    controller: "defaultViewController"
+                })
+                .when("/resume",{
+                    templateUrl: "/angular/views/user/resume.view.client.html",
+                    controller: "resumeViewController",
+                    controllerAs: "webCon"
+                })
+                .otherwise({
+                    redirectTo: "/"
+                });
+        });
+})();
+
